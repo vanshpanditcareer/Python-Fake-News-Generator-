76ff13ba4726341ee77e34e990ec09fb4109b413
vanshpanditcarrer <vanshpandit.carrer@gmail.com>
2025-09-03 00:52:31 +0530

    Done

--- Project.py
+++ Project.py
@@ -1,1 +1,47 @@
-print ("Hello Git")
+#1- Import the random module
+import random
+
+#create subjects
+subjects = [
+    "Shahruk Khan",
+    "Virat Kohli",
+    "Nirmala Sitaraman",
+    "MS Dhoni",
+    "A group of monkeys",
+    "Prime Minister Modi",
+]
+
+actions = [
+    "launches",
+    "cancels",
+    "eats",
+    "scored century",
+    "passed bill",
+    "declares war on",
+    "celebrates",
+]
+
+places_or_things = [
+    "at red fort",
+    "mumbai local train",
+    "a plate of samosa",
+    "inside parliament",
+    "at Ganga ghat",
+    "during IPL Match",
+]
+
+#start the headline generation loop
+while True:
+    subject = random.choice(subjects)
+    action = random.choice(actions)
+    place_or_thing = random.choice(places_or_things)
+
+    headline = f" Breaking News: {subject} {action} {place_or_thing}"
+    print("\n" + headline)
+
+    userinput = input("\nDo you want another headline? (yes/no): ").strip().lower()
+    if userinput == "no":
+        break
+
+#goodbye message
+print("\nThanks for using, Have a fun day!")
